f86b2a463c3302fc21cdc5a6fa92ef393b6f878e
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-26 18:18:33 +0300

    Add DB connection

---
+++ internal/db/connection.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/NikolaySimakov/avito-go/config"
+)
+
+func Database() *sql.DB {
+	// get connection info
+	dbConfig := config.Database()
+
+	// open DataBase
+	db, err := sql.Open("postgres", dbConfig.GetURL())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
