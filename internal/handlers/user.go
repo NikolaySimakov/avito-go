6fe548b9a3a8c333747bedd88ec923393357475e
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-26 16:49:55 +0300

    Init handlers

---
+++ internal/handlers/user.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func Add(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetSegments(w http.ResponseWriter, r *http.Request) {
+	// var id int
+
+	// returns segments.Segments
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete() {
+
+}
