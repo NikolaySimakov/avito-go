60c28ba5cc8b24f76ae389744d28f79f31a060d4
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-31 15:44:05 +0300

    Add db errors

---
+++ internal/db/errors/errors.go
@@ -0,0 +1,9 @@
+package errors
+
+import "errors"
+
+var (
+	ErrNotFound         = errors.New("not found")
+	ErrAlreadyExists    = errors.New("already exists")
+	ErrSegmentsNotExist = errors.New("no segments")
+)
