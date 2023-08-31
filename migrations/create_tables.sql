8d01e92fc7ad95b4cbab29c7ac81d8bf6c1d632e
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-31 14:08:48 +0300

    Add DB migrations

---
+++ migrations/create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+  user_id VARCHAR(40) PRIMARY KEY,
+  created_at TIMESTAMP not null default now()
+);
+
+CREATE TABLE segments (
+  slug VARCHAR PRIMARY KEY,
+  created_at TIMESTAMP not null default now()
+);
+
+CREATE TABLE user_segments (
+  user_id VARCHAR(40) REFERENCES users(user_id),
+  segment_slug VARCHAR REFERENCES segments(slug) ON DELETE CASCADE,
+  PRIMARY KEY (user_id, segment_slug)
+);
---
+++ migrations/drop_tables.sql
@@ -0,0 +1,5 @@
+drop table  if exists users;
+
+drop table if exists segments;
+
+drop table if exists user_segments;
